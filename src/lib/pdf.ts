6e59e1acc26cd1d45ec1943a5f0a0c711f9d8096
adhEngineer <adh.medeng@gmail.com>
2026-05-07 15:56:07 +0300

    Move PDF note text closer to heading

--- src/lib/pdf.ts
+++ src/lib/pdf.ts
@@ -60,18 +60,18 @@ export async function generateOrderPdf(data: OrderFormData) {
 
   page1.drawRectangle({
     x: 92,
-    y: 296,
+    y: 298,
     width: 468,
-    height: 90,
+    height: 96,
     color: rgb(1, 1, 1),
   });
-  drawBlackText(page1, '- conform contract educational, setul de uniforme este compus din 2 tricouri cu maneca', 100, 358, 9.6, font);
-  drawBlackText(page1, 'scurta si 2 tricouri cu maneca lunga in valoare de 650lei/set.', 100, 345, 9.6, font);
-  drawBlackText(page1, '- la solicitarea pe bucata (exceptand setul conform contract) pretul este:', 100, 327, 9.6, font);
-  page1.drawCircle({ x: 120, y: 317, size: 1.8, color: rgb(0, 0, 0) });
-  page1.drawCircle({ x: 120, y: 304, size: 1.8, color: rgb(0, 0, 0) });
-  drawBlackText(page1, 'tricou cu maneca scurta - 150lei/buc.', 128, 314, 9.6, font);
-  drawBlackText(page1, 'tricou cu maneca lunga - 175lei/buc.', 128, 301, 9.6, font);
+  drawBlackText(page1, '- conform contract educational, setul de uniforme este compus din 2 tricouri cu maneca', 100, 375, 9.6, font);
+  drawBlackText(page1, 'scurta si 2 tricouri cu maneca lunga in valoare de 650lei/set.', 100, 362, 9.6, font);
+  drawBlackText(page1, '- la solicitarea pe bucata (exceptand setul conform contract) pretul este:', 100, 344, 9.6, font);
+  page1.drawCircle({ x: 120, y: 334, size: 1.8, color: rgb(0, 0, 0) });
+  page1.drawCircle({ x: 120, y: 321, size: 1.8, color: rgb(0, 0, 0) });
+  drawBlackText(page1, 'tricou cu maneca scurta - 150lei/buc.', 128, 331, 9.6, font);
+  drawBlackText(page1, 'tricou cu maneca lunga - 175lei/buc.', 128, 318, 9.6, font);
 
   page2.setFont(bold);
   drawText(page2, data.signature_name, 232, 56, 15, bold);
